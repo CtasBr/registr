86254356176ebf0eb6f7cbe56603b7f5a9ee05c5
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-11 18:11:09 +0300

    Update regs.php

--- regs.php
+++ regs.php
@@ -1,21 +1,42 @@
 <?php
-require 'vendor/autoload.php'; 
+echo "<link rel='stylesheet' href='regphp.css'>";
+$nm = $_POST["nm"];
+$em = $_POST["em"];
+$nc = $_POST["nc"];
+$np = $_POST["np"];
+$ncp = $_POST["ncp"];
 
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("st.dm.br.04@mail.ru", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("st.dm.br.04@mai.ru", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('stas'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+$nm = htmlspecialchars($nm);
+$em = htmlspecialchars($em);
+$nc = htmlspecialchars($nc);
+$np = htmlspecialchars($np);
+$ncp = htmlspecialchars($ncp);
+
+$nm = urldecode($nm);
+$em = urldecode($em);
+$nc = urldecode($nc);
+$np = urldecode($np);
+$ncp = urldecode($ncp);
+
+$nm = trim($nm);
+$em = trim ($em);
+$nc = trim($nc);
+$np = trim ($np);
+$ncp = trim ($ncp);
+
+/*echo $nm;
+echo "<br>";
+echo $em;
+echo "<br>";
+echo $nc;
+echo "<br>";
+echo $np;
+echo "<br>";
+echo $ncp;*/
+
+if (mail ("st.dm.br.04@mail.ru" ,"syte" ,"FIO:".$nm.". Email:".$em.". Номер машины:".$nc.". Номер паспорта:".$np.". Номер прав:".$ncp ,"From: stas.bratkov@bk.ru \r\n")){
+	echo "<a>Отправлено</a><a href=\'index.php\'> на главную</a>";
+} else {
+	echo "<a>Ошибка</a>";
 }
 ?>
