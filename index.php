6287c689862e5aaa1c5a285c731deef0956a9023
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-11 18:55:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,47 @@
-<?php
-echo "export SENDGRID_API_KEY='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'" > sendgrid.env
-echo "sendgrid.env" >> .gitignore;
-source ./sendgrid.env;
-require 'vendor/autoload.php';
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("stanislavdmbr@gmail.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("stanislavdmbr@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(getenv('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'));
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-}
-?>
+<html>
+<head>
+<link rel="stylesheet" href="reg.css">
+<title>Анкета страховой</title>
+</head>
+<body>
+	<form action="regs.php" method="post">
+		<h1>Анкета для страховой компании</h1>
+	
+	<div class="group">
+		<label>Фамилия Имя Отчество:</label><br>
+		<input type="text" name="nm" placeholder="ФИО" required>
+	</div>
+	<div class="group">
+		<label>Адрес электронной почты:</label><br>
+		<input type="email" name="em" placeholder="Email" required>
+	</div>
+	<div class="group">
+		<label>Номер машины:</label><br>
+		<input type="text" name="nc" placeholder="Номер машины" required>
+	</div>
+	<div class="group">
+		<label>Номер паспорта:</label><br>
+		<input type="text" name="np" placeholder="Номер паспорта" required>
+	</div>
+	<div class="group">
+		<label>номер водительского удоставерения:</label><br>
+		<input type="text" name="ncp" placeholder="Номер водительского удостоверения" required>
+	</div>
+	<div>
+		<button type="submit" name="Sand">Отправить заявку</button> 
+	</div>
+	</form>
+
+<div id="save">SAVE</div>
+<div id="save1">SAVE</div>
+<div id="save2">SAVE</div>
+<div id="save3">SAVE</div>
+<div id="save4">SAVE</div>
+<div id="save5">SAVE</div>
+<div id="save6">SAVE</div>
+<div id="save7">SAVE</div>
+
+
+
+</body>
+</html>
