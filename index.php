506a3ee861d265e106c32809a378ca68f89e0773
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-11 18:43:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+require 'vendor/autoload.php';
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("test@example.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
@@ -15,7 +8,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+$sendgrid = new \SendGrid(getenv('stas'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
@@ -23,4 +16,5 @@ try {
     print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
-}?>
+}
+?>
