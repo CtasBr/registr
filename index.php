69fb411f046ca5b0626e9bb10045c65a3205e445
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-11 18:49:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "export SENDGRID_API_KEY='stas'" > sendgrid.env
+echo "export SENDGRID_API_KEY='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'" > sendgrid.env
 echo "sendgrid.env" >> .gitignore
 source ./sendgrid.env;
 require 'vendor/autoload.php';
@@ -11,7 +11,7 @@ $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid(getenv('stas'));
+$sendgrid = new \SendGrid(getenv('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'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
