cbc1e74599a552bf38c7ce3d0c8b686dc4f15680
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-11 17:30:57 +0300

    Update regs.php

--- regs.php
+++ regs.php
@@ -1,12 +1,6 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
+require 'vendor/autoload.php'; 
+
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("st.dm.br.04@mail.ru", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
