5dd64630740278a0b193d7cb5f5f8f9899d8666d
CtasBr <48413588+CtasBr@users.noreply.github.com>
2019-03-11 21:36:40 +0300

    Update regh.php

--- regh.php
+++ regh.php
@@ -1,18 +1,17 @@
 <?php
+// using SendGrid PHP Library
+// https://github.com/sendgrid/sendgrid-php
 require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
-
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("st.dm.br.04@mail.ru", "Example User");
+// require("./sendgrid-php.php");
+// If not using Composer, uncomment the above line
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("test@example.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("st.dm.br.04@mail.ru", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addTo("test@example.com", "Example User");
+$email->addContent(
+    "text/plain", "and easy to do anywhere, even with PHP"
+);
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
@@ -23,6 +22,5 @@ try {
     print_r($response->headers());
     print $response->body() . "\n";
 } catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
-?>
